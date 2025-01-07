31d231a8cc9521bc158a17bad64f003fbd765077
Mads A. Pedersen <mads.amtoft@hotmail.com>
2025-01-07 17:11:32 +0100

    Confused about the shift immediates, NEED HELP!

--- src/main/scala/ALU.scala
+++ src/main/scala/ALU.scala
@@ -14,6 +14,7 @@ class ALU extends Module{
   val io = IO(new Bundle{
     val input = Input(new ALUFields())
     val result = Output(SInt(32.W))
+    val imm = Output(SInt(32.W))
   })
 
   // Instruction Types
@@ -80,14 +81,14 @@ class ALU extends Module{
         }
         is("0x2".U) {
           switch(io.input.funct7) {
-            is("0x00".U) {
+            is("0x00".U) { //Set Less Than
               io.result := Mux(io.input.data1 < io.input.data2, 1.S(32.W), 0.S(32.W))
             }
           }
         }
         is("0x3".U) {
           switch(io.input.funct7) {
-            is("0x00".U) {
+            is("0x00".U) { //Set Less Than (Unsigned)
               io.result := Mux(io.input.data1.asUInt < io.input.data2.asUInt, 1.S(32.W), 0.S(32.W))
             }
           }
@@ -96,15 +97,45 @@ class ALU extends Module{
     }
 
     is(I_Type_1, I_Type_2, I_Type_3, I_Type_4) {
-
+      switch(io.input.funct3) {
+        is("0x0".U) { //ADD Immediate
+          io.result := io.input.data1 + io.input.imm
+        }
+        is("0x4".U) { //XOR Immediate
+          io.result := io.input.data1 ^ io.input.imm
+        }
+        is("0x6".U) { //OR Immediate
+          io.result := io.input.data1 | io.input.imm
+        }
+        is("0x7".U) { //AND Immediate
+          io.result := io.input.data1 & io.input.imm
+        }
+        is("0x1".U) { //Shift Left Logical Imm
+          io.result := io.input.data1 << io.input.imm(4, 0)
+          io.imm := Cat(("0x00".U(7.W)) + io.input.imm(4, 0))
+        }
+        is("0x5".U) {
+          io.result := 0.S
+        }
+        is("0x2".U) {
+          io.result := 0.S
+        }
+        is("0x3".U) {
+          io.result := 0.S
+        }
+      }
     }
 
     is(S_Type) {
+      switch(io.input.funct3) {
 
+      }
     }
 
     is(B_Type) {
+      switch(io.input.funct3) {
 
+      }
     }
 
     is(U_Type) {
