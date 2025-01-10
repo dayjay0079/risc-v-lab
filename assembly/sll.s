efc0bf6418d305c7bfd4aacdc95793e2ea5d6693
Dejan <deja0079@outlook.dk>
2025-01-10 11:13:09 +0100

    Fix pc

---
+++ assembly/sll.s
@@ -0,0 +1,17 @@
+nop
+addi x1, x0, 5
+addi x2, x0, 2
+nop
+nop
+nop
+nop
+sll x3, x1, x2
+nop
+nop
+nop
+nop
+slli x4, x1, 3
+nop
+nop
+nop
+nop
--- src/main/scala/Stage1_IF.scala
+++ src/main/scala/Stage1_IF.scala
@@ -13,9 +13,15 @@ class Stage1_IF(program: Seq[Int], fpga: Boolean) extends Module {
   val instruction_memory = Module(new MemoryInstruction(program, fpga))
 
   // Set up program counter circuit
+  val isfirst = RegInit(0.B)
   val pc_reg = RegInit(-4.S(32.W))
-  val pc = Mux(io.jump, (pc_reg + io.jump_offset).asUInt, (pc_reg + 4.S).asUInt)
+  val pc = Mux(io.jump, (pc_reg + io.jump_offset).asUInt, Mux(isfirst, (pc_reg + 4.S).asUInt, 0.U))
   pc_reg := pc.asSInt
+
+  when(!isfirst) {
+    isfirst := 1.B
+  }
+
 
   // Read instruction
   instruction_memory.io.pc := pc
