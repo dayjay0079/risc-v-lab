4dc64c3bc904e5e490e178bfdd75aa7aa966df44
Dejan <deja0079@outlook.dk>
2025-01-10 08:21:54 +0100

    Slight rework

--- src/main/scala/Stage1_IF.scala
+++ src/main/scala/Stage1_IF.scala
@@ -9,13 +9,18 @@ class Stage1_IF(program: Seq[Int], fpga: Boolean) extends Module {
     val pc = Output(UInt(32.W))
   })
 
-  val im = Module(new MemoryInstruction(program, fpga))
+  // Initialize instruction memory with given program
+  val instruction_memory = Module(new MemoryInstruction(program, fpga))
+
+  // Set up program counter circuit
   val pc_reg = RegInit(-4.S(32.W))
   val pc = Mux(io.jump, (pc_reg + io.jump_offset).asUInt, (pc_reg + 4.S).asUInt)
   pc_reg := pc.asSInt
-  //pc_reg := -4.S
 
-  im.io.pc := pc
-  io.instruction := im.io.instruction
+  // Read instruction
+  instruction_memory.io.pc := pc
+
+  // Output
+  io.instruction := instruction_memory.io.instruction
   io.pc := pc
 }
