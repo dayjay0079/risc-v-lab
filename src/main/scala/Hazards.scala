9de496c0b37bf44c2994c1192846e9440d3b4799
Don-Tun <val.neerg@gmail.com>
2025-01-16 11:34:48 +0100

    Stall bools correctly implemented, EX_control always 0.

--- src/main/scala/Hazards.scala
+++ src/main/scala/Hazards.scala
@@ -1,6 +1,7 @@
-import chisel3._
+import chisel3.{when, _}
 import chisel3.util._
 import chisel3.util.experimental.decode.decoder
+import lib.ControlBus
 
 class HazardInfo extends Bundle {
   val rd = UInt(5.W) // Destination register ID
@@ -11,12 +12,12 @@ class Hazards extends Module{
     val rs1 = Input(UInt(5.W))
     val rs2 = Input(UInt(5.W))
     val rd = Input(UInt(5.W))
-    val input = Input(new PipelineValuesEX)
+    val ctrl = Input(new ControlBus)
 
     // Outputs
     val EX_control = Output(UInt(4.W))
-    val stall_IF = Wire(Bool())
-    val stall_ID = Wire(Bool())
+    val stall_IF = Output(Bool())
+    val stall_ID = Output(Bool())
   })
 
   // instruction types:
@@ -30,112 +31,94 @@ class Hazards extends Module{
   val U_Type_1 = "b0110111".U // lui
   val U_Type_2 = "b0010111".U // auipc
 
-  // Stall booleans:
-
-  val stall_counter = RegInit(0.U(2.W)) // 2-bit counter for double stall
-  val stall_active = Wire(Bool())
-
-  stall_active := stall_counter =/= 0.U
-  io.stall_IF := false.B
-  io.stall_ID := false.B
 
   // Registers tracking instructions for other stages
   val hz_ID = Wire(new HazardInfo)
   hz_ID.rd := io.rd
-  hz_ID.opcode := io.input.ctrl.opcode
+  hz_ID.opcode := io.ctrl.opcode
 
   val hz_EX = RegNext(hz_ID) // Saving EX info
   val hz_MEM = RegNext(hz_EX) // MEM info
   val hz_WB = RegNext(hz_MEM) // ...
 
+  // Stall booleans:
+  val stall_counter = RegInit(0.U(1.W)) // 2-bit counter for double stall
+  io.stall_ID := false.B
+  io.stall_IF := false.B
+
+  // Decrement with 1 pr cycle and stall_IF to true
+  when(stall_counter > 0.U) {
+    stall_counter := stall_counter - 1.U
+    io.stall_IF := true.B
+  }
 
   // Stall ID and IF once for load-use hazard
   when(hz_EX.opcode === I_Type_2 && (hz_EX.rd === io.rs1 || hz_EX.rd === io.rs2)) {
     io.stall_ID := true.B
-    stall_counter += 1
+    io.stall_IF := true.B
   }
 
   // Stall IF twice for Branching (not branch-prediction compatible)
   when(hz_ID.opcode === B_Type) {
-    stall_counter += 1
+    io.stall_IF := true.B
+    stall_counter := 1.U
   }
 
-  // Always stall IF if counter active
-  io.stall_IF := stall_counter
-
-
-
-  // 16 combinations of rs1/rs2 overwrites (0 = no change)
-  io.EX_control := 0.U
 
   // if statements for 4 bit control signal for EX stage
-  when (hz_WB.opcode === (R_Type | I_Type_1 | I_Type_2 | I_Type_3 | U_Type_1 | U_Type_2)) {
-    when((hz_WB.rd === io.rs1) & (hz_WB.rd === io.rs2)) {
-      io.EX_control := 9.U // For WB_rd / WB_rd
-    }
-    when(hz_MEM.rd === io.rs1) {
-      io.EX_control := 6.U // For WB_rd / rs2
-    }
-
-    when(hz_MEM.rd === io.rs2) {
-      io.EX_control := 3.U // For rs1 / WB_rd
-    }
-  }
-
-  when (hz_MEM.opcode === (R_Type | I_Type_1 | I_Type_2 | I_Type_3 | U_Type_1 | U_Type_2)) {
-    when((hz_MEM.rd === io.rs1) & (hz_MEM.rd === io.rs2)) {
-      io.EX_control := 8.U // For MEM_rd / MEM_rd
-    }
-    when(hz_MEM.rd === io.rs1) {
-      when(hz_WB.opcode === (R_Type | I_Type_1 | I_Type_2 | I_Type_3 | U_Type_1 | U_Type_2)) {
-        when(hz_WB.rd === io.rs2) {
-          io.EX_control := 14.U // For MEM_rd / WB_rd
-        }
-      }
-      io.EX_control := 5.U // For MEM_rd / rs2
-    }
-
-    when(hz_MEM.rd === io.rs2) {
-      when(hz_WB.opcode === (R_Type | I_Type_1 | I_Type_2 | I_Type_3 | U_Type_1 | U_Type_2)) {
-        when(hz_WB.rd === io.rs1) {
-          io.EX_control := 15.U // For WB_rd / MEM_rd
-        }
-      }
-      io.EX_control := 2.U // For rs1 / MEM_rd
-    }
-  }
-
-  when (hz_EX.opcode === (R_Type | I_Type_1 | I_Type_2 | I_Type_3 | U_Type_1 | U_Type_2)) {
+  when(hz_EX.opcode === (R_Type | I_Type_1 | I_Type_2 | I_Type_3 | U_Type_1 | U_Type_2)) {
     when((hz_EX.rd === io.rs1) & (hz_EX.rd === io.rs2)) {
       io.EX_control := 7.U // For EX_rd / EX_rd
-    }
-    when(hz_EX.rd === io.rs1) {
+    } .elsewhen(hz_EX.rd === io.rs1) {
       when(hz_MEM.opcode === (R_Type | I_Type_1 | I_Type_2 | I_Type_3 | U_Type_1 | U_Type_2)) {
         when(hz_MEM.rd === io.rs2) {
           io.EX_control := 10.U // For EX_rd / MEM_rd
         }
-      }
-      when(hz_WB.opcode === (R_Type | I_Type_1 | I_Type_2 | I_Type_3 | U_Type_1 | U_Type_2)) {
+      } .elsewhen(hz_WB.opcode === (R_Type | I_Type_1 | I_Type_2 | I_Type_3 | U_Type_1 | U_Type_2)) {
         when(hz_WB.rd === io.rs2) {
           io.EX_control := 11.U // For EX_rd / WB_rd
         }
-      }
-      io.EX_control := 4.U // For EX_rd / rs2
-    }
-
-    when(hz_EX.rd === io.rs2) {
+      } .otherwise {io.EX_control := 4.U} // For EX_rd / rs2
+    } .elsewhen(hz_EX.rd === io.rs2) {
       when(hz_MEM.opcode === (R_Type | I_Type_1 | I_Type_2 | I_Type_3 | U_Type_1 | U_Type_2)) {
         when(hz_MEM.rd === io.rs1) {
           io.EX_control := 12.U // For MEM_rd / EX_rd
         }
-      }
-      when(hz_WB.opcode === (R_Type | I_Type_1 | I_Type_2 | I_Type_3 | U_Type_1 | U_Type_2)) {
+      } .elsewhen(hz_WB.opcode === (R_Type | I_Type_1 | I_Type_2 | I_Type_3 | U_Type_1 | U_Type_2)) {
         when(hz_WB.rd === io.rs1) {
           io.EX_control := 13.U // For WB_rd / EX_rd
         }
+      } .otherwise {
+        io.EX_control := 1.U // For rs1 / EX_rd
       }
-      io.EX_control := 1.U // For rs1 / EX_rd
     }
-  }
-
+  } .elsewhen (hz_MEM.opcode === (R_Type | I_Type_1 | I_Type_2 | I_Type_3 | U_Type_1 | U_Type_2)) {
+    when((hz_MEM.rd === io.rs1) & (hz_MEM.rd === io.rs2)) {
+      io.EX_control := 8.U // For MEM_rd / MEM_rd
+    }.elsewhen(hz_MEM.rd === io.rs1) {
+      when(hz_WB.opcode === (R_Type | I_Type_1 | I_Type_2 | I_Type_3 | U_Type_1 | U_Type_2)) {
+        when(hz_WB.rd === io.rs2) {
+          io.EX_control := 14.U // For MEM_rd / WB_rd
+        }
+      }.otherwise {
+        io.EX_control := 5.U // For MEM_rd / rs2
+      }
+    }
+  }.elsewhen(hz_MEM.rd === io.rs2) {
+    when(hz_WB.opcode === (R_Type | I_Type_1 | I_Type_2 | I_Type_3 | U_Type_1 | U_Type_2)) {
+      when(hz_WB.rd === io.rs1) {
+        io.EX_control := 15.U // For WB_rd / MEM_rd
+      }
+    }.otherwise {
+      io.EX_control := 2.U // For rs1 / MEM_rd
+    }
+  } .elsewhen (hz_WB.opcode === (R_Type | I_Type_1 | I_Type_2 | I_Type_3 | U_Type_1 | U_Type_2)) {
+    when((hz_WB.rd === io.rs1) & (hz_WB.rd === io.rs2)) {
+      io.EX_control := 9.U // For WB_rd / WB_rd
+    }.elsewhen(hz_WB.rd === io.rs1) {
+      io.EX_control := 6.U // For WB_rd / rs2
+    }.elsewhen(hz_WB.rd === io.rs2) {
+      io.EX_control := 3.U // For rs1 / WB_rd
+    }
+  } .otherwise {io.EX_control := 0.U} // Base case
 }
---
+++ src/test/scala/HazardTest.scala
@@ -0,0 +1,21 @@
+import chisel3._
+import chiseltest._
+import lib.ReadAssembly
+import org.scalatest.flatspec.AnyFlatSpec
+class HazardTest extends AnyFlatSpec with ChiselScalatestTester {
+  "Hazard Test" should "pass" in {
+    val FPGA = false
+    val MEM_SIZE = 1024
+    val FREQ = 50000000
+    val BAUD = 9600
+    val PROGRAM: Seq[Int] = ReadAssembly.readBin("assembly/Hz_test.bin")
+    test(new Top(PROGRAM, FPGA, MEM_SIZE, FREQ, BAUD)).withAnnotations(Seq(WriteVcdAnnotation)) { dut =>
+      val pc_max = 0x38
+      while (dut.io.pc.peekInt <= pc_max) {
+        dut.clock.step()
+      }
+      // dut.clock.step(50)
+    }
+  }
+}
+
