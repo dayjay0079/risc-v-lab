890ebba0da1ed3964a68f516c894e7392c80e9aa
Don-Tun <val.neerg@gmail.com>
2025-01-16 09:28:22 +0100

    removed redundant switches. Not tested

--- src/main/scala/Hazards.scala
+++ src/main/scala/Hazards.scala
@@ -68,79 +68,73 @@ class Hazards extends Module{
   // 16 combinations of rs1/rs2 overwrites (0 = no change)
   io.EX_control := 0.U
 
-  // switches for 4 bit control signal for EX stage
-  switch(hz_WB.opcode) {
-    is(R_Type | I_Type_1 | I_Type_2 | I_Type_3 | U_Type_1 | U_Type_2) {
-      when((hz_WB.rd === io.rs1) & (hz_WB.rd === io.rs2)) {
-        io.EX_control := 9.U // For WB_rd / WB_rd
-      }
-      when(hz_MEM.rd === io.rs1) {
-        io.EX_control := 6.U // For WB_rd / rs2
-      }
+  // if statements for 4 bit control signal for EX stage
+  when (hz_WB.opcode === (R_Type | I_Type_1 | I_Type_2 | I_Type_3 | U_Type_1 | U_Type_2)) {
+    when((hz_WB.rd === io.rs1) & (hz_WB.rd === io.rs2)) {
+      io.EX_control := 9.U // For WB_rd / WB_rd
+    }
+    when(hz_MEM.rd === io.rs1) {
+      io.EX_control := 6.U // For WB_rd / rs2
+    }
 
-      when(hz_MEM.rd === io.rs2) {
-        io.EX_control := 3.U // For rs1 / WB_rd
-      }
+    when(hz_MEM.rd === io.rs2) {
+      io.EX_control := 3.U // For rs1 / WB_rd
     }
   }
 
-  switch(hz_MEM.opcode) {
-    is(R_Type | I_Type_1 | I_Type_2 | I_Type_3 | U_Type_1 | U_Type_2) {
-      when((hz_MEM.rd === io.rs1) & (hz_MEM.rd === io.rs2)) {
-        io.EX_control := 8.U // For MEM_rd / MEM_rd
-      }
-      when(hz_MEM.rd === io.rs1) {
-        when(hz_WB.opcode === (R_Type | I_Type_1 | I_Type_2 | I_Type_3 | U_Type_1 | U_Type_2)) {
-          when(hz_WB.rd === io.rs2) {
-            io.EX_control := 14.U // For MEM_rd / WB_rd
-          }
+  when (hz_MEM.opcode === (R_Type | I_Type_1 | I_Type_2 | I_Type_3 | U_Type_1 | U_Type_2)) {
+    when((hz_MEM.rd === io.rs1) & (hz_MEM.rd === io.rs2)) {
+      io.EX_control := 8.U // For MEM_rd / MEM_rd
+    }
+    when(hz_MEM.rd === io.rs1) {
+      when(hz_WB.opcode === (R_Type | I_Type_1 | I_Type_2 | I_Type_3 | U_Type_1 | U_Type_2)) {
+        when(hz_WB.rd === io.rs2) {
+          io.EX_control := 14.U // For MEM_rd / WB_rd
         }
-        io.EX_control := 5.U // For MEM_rd / rs2
       }
+      io.EX_control := 5.U // For MEM_rd / rs2
+    }
 
-      when(hz_MEM.rd === io.rs2) {
-        when(hz_WB.opcode === (R_Type | I_Type_1 | I_Type_2 | I_Type_3 | U_Type_1 | U_Type_2)) {
-          when(hz_WB.rd === io.rs1) {
-            io.EX_control := 15.U // For WB_rd / MEM_rd
-          }
+    when(hz_MEM.rd === io.rs2) {
+      when(hz_WB.opcode === (R_Type | I_Type_1 | I_Type_2 | I_Type_3 | U_Type_1 | U_Type_2)) {
+        when(hz_WB.rd === io.rs1) {
+          io.EX_control := 15.U // For WB_rd / MEM_rd
         }
-        io.EX_control := 2.U // For rs1 / MEM_rd
       }
+      io.EX_control := 2.U // For rs1 / MEM_rd
     }
   }
 
-  switch(hz_EX.opcode) {
-    is(R_Type | I_Type_1 | I_Type_2 | I_Type_3 | U_Type_1 | U_Type_2) {
-      when((hz_EX.rd === io.rs1) & (hz_EX.rd === io.rs2)) {
-        io.EX_control := 7.U // For EX_rd / EX_rd
-      }
-      when(hz_EX.rd === io.rs1) {
-        when(hz_MEM.opcode === (R_Type | I_Type_1 | I_Type_2 | I_Type_3 | U_Type_1 | U_Type_2)) {
-          when(hz_MEM.rd === io.rs2) {
-            io.EX_control := 10.U // For EX_rd / MEM_rd
-          }
+  when (hz_EX.opcode === (R_Type | I_Type_1 | I_Type_2 | I_Type_3 | U_Type_1 | U_Type_2)) {
+    when((hz_EX.rd === io.rs1) & (hz_EX.rd === io.rs2)) {
+      io.EX_control := 7.U // For EX_rd / EX_rd
+    }
+    when(hz_EX.rd === io.rs1) {
+      when(hz_MEM.opcode === (R_Type | I_Type_1 | I_Type_2 | I_Type_3 | U_Type_1 | U_Type_2)) {
+        when(hz_MEM.rd === io.rs2) {
+          io.EX_control := 10.U // For EX_rd / MEM_rd
         }
-        when(hz_WB.opcode === (R_Type | I_Type_1 | I_Type_2 | I_Type_3 | U_Type_1 | U_Type_2)) {
-          when(hz_WB.rd === io.rs2) {
-            io.EX_control := 11.U // For EX_rd / WB_rd
-          }
-        }
-        io.EX_control := 4.U // For EX_rd / rs2
       }
+      when(hz_WB.opcode === (R_Type | I_Type_1 | I_Type_2 | I_Type_3 | U_Type_1 | U_Type_2)) {
+        when(hz_WB.rd === io.rs2) {
+          io.EX_control := 11.U // For EX_rd / WB_rd
+        }
+      }
+      io.EX_control := 4.U // For EX_rd / rs2
+    }
 
-      when(hz_EX.rd === io.rs2) {
-        when(hz_MEM.opcode === (R_Type | I_Type_1 | I_Type_2 | I_Type_3 | U_Type_1 | U_Type_2)) {
-          when(hz_MEM.rd === io.rs1) {
-            io.EX_control := 12.U // For MEM_rd / EX_rd
-          }
+    when(hz_EX.rd === io.rs2) {
+      when(hz_MEM.opcode === (R_Type | I_Type_1 | I_Type_2 | I_Type_3 | U_Type_1 | U_Type_2)) {
+        when(hz_MEM.rd === io.rs1) {
+          io.EX_control := 12.U // For MEM_rd / EX_rd
         }
-        when(hz_WB.opcode === (R_Type | I_Type_1 | I_Type_2 | I_Type_3 | U_Type_1 | U_Type_2)) {
-          when(hz_WB.rd === io.rs1) {
-            io.EX_control := 13.U // For WB_rd / EX_rd
-          }
-        }
-        io.EX_control := 1.U // For rs1 / EX_rd
       }
+      when(hz_WB.opcode === (R_Type | I_Type_1 | I_Type_2 | I_Type_3 | U_Type_1 | U_Type_2)) {
+        when(hz_WB.rd === io.rs1) {
+          io.EX_control := 13.U // For WB_rd / EX_rd
+        }
+      }
+      io.EX_control := 1.U // For rs1 / EX_rd
     }
   }
 
