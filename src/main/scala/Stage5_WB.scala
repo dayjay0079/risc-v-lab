17f8754bfa6abf3e0564040157da2f64f9af6366
Mads A. Pedersen <mads.amtoft@hotmail.com>
2025-01-23 15:43:58 +0100

    WB register

--- src/main/scala/Stage5_WB.scala
+++ src/main/scala/Stage5_WB.scala
@@ -13,12 +13,16 @@ class Stage5_WB extends Module {
   val io = IO(new Bundle{
     val pipeline_vals = Input(new PipelineValuesWB)
     val data_out = Output(SInt(32.W))
+    val data_out_hz = Output(SInt(32.W))
     val rd = Output(UInt(5.W))
     val write_enable = Output(Bool())
   })
 
+  val data_out = Mux(io.pipeline_vals.ctrl.mem_to_reg, io.pipeline_vals.data_in_mem, io.pipeline_vals.data_in_alu)
+
   // Output
-  io.data_out := Mux(io.pipeline_vals.ctrl.mem_to_reg, io.pipeline_vals.data_in_mem, io.pipeline_vals.data_in_alu)
+  io.data_out := data_out
+  io.data_out_hz := RegNext(data_out)
   io.rd := io.pipeline_vals.rd
   io.write_enable := io.pipeline_vals.ctrl.write_enable_reg
 }
